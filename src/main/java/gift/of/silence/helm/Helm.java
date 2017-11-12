a34f26e3e31e49d6c694a40d5acc5802534a668e
Andrew van der Westhuizen <avanderw@gmail.com>
2017-11-12 22:13:13 +0200

    initial commit

---
+++ src/main/java/gift/of/silence/helm/Helm.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gift.of.silence.helm;
+
+/**
+ *
+ * @author Andrew
+ */
+public class Helm {
+    public void setHeading(){}
+    public void setVelocity(){}
+    public void setDepth(){}
+    public void toggleDock(){}
+}
---
+++ src/main/java/gift/of/silence/intel/Intel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gift.of.silence.intel;
+
+/**
+ *
+ * @author Andrew
+ */
+public class Intel {
+
+    public void dropSensor() {}
+    public void toggleTelescope() {}
+    public void toggleRadar(){}
+    public void toggleSonar(){}
+    public void toggleActive(){}
+}
