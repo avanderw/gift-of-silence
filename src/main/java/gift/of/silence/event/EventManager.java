3a71c2623f161aa269bcaa69b0fd469fb0bdb489
Andrew van der Westhuizen <avanderw@gmail.com>
2017-11-20 15:42:04 +0200

    added event logging

--- src/main/java/gift/of/silence/event/EventManager.java
+++ src/main/java/gift/of/silence/event/EventManager.java
@@ -6,6 +6,7 @@
 package gift.of.silence.event;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,7 +34,7 @@ public class EventManager {
     }
 
     public void notify(String eventType, Object payload) {
-        System.out.println(String.format("event{type:%s, payload:%s}", eventType, payload));
+        System.out.println(String.format("event{time:%s, type:%s, payload:%s}", new Date(), eventType, payload));
         if (listeners.get(eventType) != null) {
             listeners.get(eventType).forEach((listener) -> {
                 listener.update(eventType, payload);
